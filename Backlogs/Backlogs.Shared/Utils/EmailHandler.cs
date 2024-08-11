60c1ece6540b96ea6da0ab1a7c28508fd44b82a6
surya-sk <surya.sk05@outlook.com>
2024-08-10 20:00:54 -0500

    Implement EmailHandler

---
+++ Backlogs/Backlogs.Shared/Utils/EmailHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backlogs.Utils.Uno
+{
+    public class EmailHandler : IEmailService
+    {
+        public async Task SendEmailAsync(string subject, string body)
+        {
+            EmailMessage emailMessage = new EmailMessage();
+            emailMessage.To.Add(new EmailRecipient("surya.sk05@outlook.com"));
+            emailMessage.Subject = subject;
+            emailMessage.Body = body;
+            await EmailManager.ShowComposeNewEmailAsync(emailMessage);
+        }
+    }
+}
